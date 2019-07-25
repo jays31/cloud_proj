c0ce0e9a16224ad8515c495ff46eb7bb6d0966fe
nketia <alfred.nketia@gmail.com>
2019-07-25 12:19:18 -0300

    corrected the url in datastore

--- config/datastores.js
+++ config/datastores.js
@@ -2,5 +2,5 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-mysql',
-    hurl: 'mysql://group10@cloudprojectgrp10:Cloudgrp10@cloudprojectgrp10.mysql.database.azure.com:3306/cloudprojectgrp10', },
+    url: 'mysql://group10@cloudprojectgrp10:Cloudgrp10@cloudprojectgrp10.mysql.database.azure.com:3306/cloudprojectgrp10', },
 };
