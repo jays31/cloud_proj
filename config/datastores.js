0f9bdfb2ee702c59fc6544c6aace2041016ecc4c
nketia <alfred.nketia@gmail.com>
2019-07-25 12:58:15 -0300

    created model for RE users

--- config/datastores.js
+++ config/datastores.js
@@ -2,5 +2,7 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-mysql',
-    url: 'mysql://group10@cloudprojectgrp10:Cloudgrp10@cloudprojectgrp10.mysql.database.azure.com:3306/cloudprojectgrp10', },
-};
+  
+    url: 'mysql://group10@cloudprojectgrp10:Cloudgrp10@cloudprojectgrp10.mysql.database.azure.com:3306/cloudprojectgrp10',},
+  
+  };
